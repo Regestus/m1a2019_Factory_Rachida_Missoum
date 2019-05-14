383d4a9caf919ae5bebec91878b0ef6e2ebb5d37
Regestus <missoum.shi@gmail.com>
2019-05-14 14:51:30 +0200

    classe ProductC , dernier commit

--- src/main/java/ProductC.java
+++ src/main/java/ProductC.java
@@ -1,4 +1,11 @@
-public class ProductC {
+public class ProductC implements Product{
+    static {
+        Factory.registerProduct("ProductC", new ProductC());
+    }
+    @Override
+    public Product createProduct() {
+        return new ProductC();
+    }
     public void perform(){
         System.out.println("I am a ProductC, performing");
     }
